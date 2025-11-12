d3727e2b56e0d5a8c0a56f9a0da78f8d86a4c131
Matthew Aprilian <matthew.aprilian@gmail.com>
2025-11-12 13:41:07 +0700

    added structured auth folder and structured footer

---
+++ ProjectLelangGame/resources/views/partials/footer.blade.php
@@ -0,0 +1,43 @@
+<div class="bg-secondary-subtle d-flex flex-column justify-content-center align-items-center py-2">
+    <div class="container">
+        <div class="row gx-5 ms-3 py-4">
+            <div class="col-3 d-flex flex-column gap-4">
+                <div class="d-flex flex-column gap-2">
+                    <h5 class="text-dark fw-bold">Bantuan</h5>
+                    <a href="#" class="nav-footer">Pusat Bantuan</a>
+                </div>
+                <div class="d-flex flex-column gap-2">
+                    <h5 class="text-dark fw-bold">LelangGame</h5>
+                    <a href="#" class="nav-footer">Tentang LelangGame</a>
+                    <a href="#" class="nav-footer">Tentang LelangGame</a>
+                    <a href="#" class="nav-footer">Tentang LelangGame</a>
+                    <a href="#" class="nav-footer">Tentang LelangGame</a>
+                    <a href="#" class="nav-footer">Tentang LelangGame</a>
+                    <a href="#" class="nav-footer">Tentang LelangGame</a>
+                </div>
+            </div>
+            <div class="col-3 d-flex flex-column gap-4">
+                <div class="d-flex flex-column gap-2">
+                    <h5 class="text-dark fw-bold">Pembeli</h5>
+                    <a href="#" class="nav-footer">Cara Belanja</a>
+                    <a href="#" class="nav-footer">Cara Trading</a>
+                    <a href="#" class="nav-footer">Cara Pembayaran</a>
+                </div>
+                <div class="d-flex flex-column gap-2">
+                    <h5 class="text-dark fw-bold">Penjual</h5>
+                    <a href="#" class="nav-footer">Cara Menjadi Penjual</a>
+                    <a href="#" class="nav-footer">Metode Pengiriman Produk</a>
+                    <a href="#" class="nav-footer">Biaya Berjualan</a>
+                    <a href="#" class="nav-footer">Metode Pencairan Saldo</a>
+                    <a href="#" class="nav-footer">Testimoni Penjual</a>
+                </div>
+            </div>
+            <div class="col-3 d-flex flex-column gap-2">
+                <h5 class="text-dark fw-bold">Metode Pembayaran</h5>
+            </div>
+            <div class="col-3 d-flex flex-column gap-2">
+                <h5 class="text-dark fw-bold">Ikuti LelangGame</h5>
+            </div>
+        </div>
+    </div>
+</div>
