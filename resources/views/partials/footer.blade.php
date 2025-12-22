2c08d0eda3281cc8b3d138625132fff8d2180d6a
Bryan <bryanekasantoso@gmail.com>
2025-12-22 22:22:34 +0700

    Mengubah Tampilan Pada Login Dan Register

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,83 @@
+<footer class="my-5">
+    <div class="container-fluid">
+        <hr>
+        <div class="row px-5">
+            <div class="col-md-3 my-3">
+                <h6 class="fw-bold">Bantuan</h6>
+                <a href="#" class="text-decoration-none text-secondary link-footer">Pusat Bantuan</a>
+
+                <h6 class="mt-5 fw-bold ">LelangGame</h6>
+                <a href="#" class="text-decoration-none text-secondary link-footer">Tentang LelangGame</a><br>
+                <a href="#" class="text-decoration-none text-secondary link-footer">LelangGame Blog</a><br>
+                <a href="#" class="text-decoration-none text-secondary link-footer">Hubungi Kami</a><br>
+                <a href="#" class="text-decoration-none text-secondary link-footer">Aturan Penggunaan</a><br>
+                <a href="#" class="text-decoration-none text-secondary link-footer">Kebijakan Privasi</a><br>
+            </div>
+            <div class="col-md-3 my-3">
+                <h6 class="fw-bold">Pembeli</h6>
+                <a href="#" class="text-decoration-none text-secondary link-footer">Cara Belanja</a><br>
+                <a href="#" class="text-decoration-none text-secondary link-footer">Cara Pembayaran</a><br>
+
+                <h6 class="mt-5 fw-bold">Penjual</h6>
+                <a href="#" class="text-decoration-none text-secondary link-footer">Cara Menjadi Penjual</a><br>
+                <a href="#" class="text-decoration-none text-secondary link-footer">Biaya Berjualan</a><br>
+                <a href="#" class="text-decoration-none text-secondary link-footer">Metode Pencairan Saldo</a><br>
+                <a href="#" class="text-decoration-none text-secondary link-footer">Testimoni Penjual</a><br>
+            </div>
+            <div class="col-md-3 my-3">
+                <h6 class="fw-bold">Metode Pembayaran</h6>
+                <div class="row">
+                    <div class="col-12 my-1">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Flinkaja-sm.png&w=96&q=75" alt="" width="45">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Findodana.png&w=96&q=75" alt="" width="45" class="ms-4">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Falfamidi.png&w=96&q=75" alt="" width="45" class="ms-4">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Falfamart.png&w=96&q=75" alt="" width="45" class="ms-4">
+                    </div>
+                    <div class="col-12 my-1">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Findomaret.png&w=96&q=75" alt="" width="45">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2F3-sm.png&w=96&q=75" alt="" width="45" class="ms-4">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Findosat.png&w=96&q=75" alt="" width="45" class="ms-4">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Fxl-sm.png&w=96&q=75" alt="" width="45" class="ms-4">
+                    </div>
+                    <div class="col-12 my-1">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Ftelkomsel.png&w=96&q=75" alt="" width="45">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Flainnya.png&w=96&q=75" alt="" width="45" class="ms-4">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Fbri.png&w=96&q=75" alt="" width="45" class="ms-4">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Fpermata.png&w=96&q=75" alt="" width="45" class="ms-4">
+                    </div>
+                    <div class="col-12 my-1">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Fmaybank.png&w=96&q=75" alt="" width="45">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Fbni.png&w=96&q=75" alt="" width="45" class="ms-4">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Fmandiri.png&w=96&q=75" alt="" width="45" class="ms-4">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Fbca.png&w=96&q=75" alt="" width="45" class="ms-4">
+                    </div>
+                    <div class="col-12 my-1">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Fqris.png&w=96&q=75" alt="" width="45">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Fshopeepay.png&w=96&q=75" alt="" width="45" class="ms-4">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Fgopay.png&w=96&q=75" alt="" width="45" class="ms-4">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Fdana.png&w=96&q=75" alt="" width="45" class="ms-4">
+                    </div>
+                    <div class="col-12 my-1">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Fovo.png&w=96&q=75" alt="" width="45">
+                        <img src="https://imgop.itemku.com/?url=https%3A%2F%2Ffiles.itemku.com%2Flogo%2Fpayment%2Fdompetku-sm.png&w=96&q=75" alt="" width="45" class="ms-4">
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 my-3">
+                <h6 class="fw-bold">Ikuti LelangGame</h6>
+                    <img src="https://files.itemku.com/logo/social-media/facebook-square.svg" alt="" width="25">
+                    <img src="https://files.itemku.com/logo/social-media/instagram-square.svg" alt="" width="25" class="ms-1">
+                    <img src="https://files.itemku.com/logo/social-media/youtube-square.svg" alt="" width="25" class="ms-1">
+                    <img src="https://files.itemku.com/logo/social-media/tiktok-square.svg" alt="" width="25" class="ms-1">
+                    <img src="https://files.itemku.com/logo/social-media/twitter-square.svg" alt="" width="25" class="ms-1">
+                <h6 class="fw-bold mt-3">Download LelangGame App</h6>
+                <img src="https://imgop.itemku.com/?url=https%3A%2F%2Fitemku-assets.s3.ap-southeast-1.amazonaws.com%2Fapp%2Fgoogle-play-badge.png&w=375&q=75" alt="" width="130" class="mt-2">
+            </div>
+        </div>
+        <hr>
+        <p class="text-center text-secondary">
+            LelangGame © 2025. All Rights Reserved. <br>
+            All trademarks belong to their respective owners.
+        </p>
+    </div>
+</footer>
---
+++ storage/framework/views/3761694191c6b9e6ab798a99293c7da4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?php echo e(config('app.name')); ?></title>
+</head>
+
+<body>
+    <p>Kode otp anda adalah: <?php echo e($otp); ?> </p>
+</body>
+
+</html>
+<?php /**PATH D:\xampp\htdocs\ProjectLelangGame\resources\views/emails/otp.blade.php ENDPATH**/ ?>
