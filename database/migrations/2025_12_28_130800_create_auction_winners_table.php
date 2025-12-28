d9fb337fa0c6cd2110140d1bc9550f85c933dc38
Vincents13 <6327.vincent@gmail.com>
2025-12-28 15:21:17 +0700

    migration dan model lelang

---
+++ database/migrations/2025_12_28_130800_create_auction_winners_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('auction_winners', function (Blueprint $table) {
+            $table->id('winner_id');
+            $table->foreignId('auction_id')->constrained('auctions', 'auction_id')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users', 'user_id')->cascadeOnDelete();
+            $table->integer('winning_price');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('auction_winners');
+    }
+};
