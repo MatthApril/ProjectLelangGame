920f60fe87484b1a1cd65db557b0c1f0722cc046
Bryan <bryanekasantoso@gmail.com>
2026-01-10 00:29:59 +0700

    Merubah Dan Menambahkan Navigasi Di Footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -4,25 +4,27 @@
         <div class="row">
             <div class="col-md-3 my-3">
                 <h6 class="fw-bold">Bantuan</h6>
-                <a href="#" class="text-decoration-none text-secondary link-footer">Pusat Bantuan</a>
-
-                <h6 class="mt-5 fw-bold ">LelangGame</h6>
-                <a href="#" class="text-decoration-none text-secondary link-footer">Tentang LelangGame</a><br>
-                <a href="#" class="text-decoration-none text-secondary link-footer">LelangGame Blog</a><br>
-                <a href="#" class="text-decoration-none text-secondary link-footer">Hubungi Kami</a><br>
-                <a href="#" class="text-decoration-none text-secondary link-footer">Aturan Penggunaan</a><br>
-                <a href="#" class="text-decoration-none text-secondary link-footer">Kebijakan Privasi</a><br>
+                <a href="{{ route('user.services') }}" class="text-decoration-none text-secondary link-footer">Layanan</a><br>
+                <a href="" class="text-decoration-none text-secondary link-footer" data-bs-toggle="modal" data-bs-target="#termsConditions">Syarat & Ketentuan</a><br>
+                <a href="readme_img/GuideBook.pdf?raw=true" download="LelangGame_GuideBook.pdf" class="text-decoration-none text-secondary link-footer">Buku Bantuan</a>
+                <h6 class="fw-bold mt-5">Navigasi Pengguna</h6>
+                <a href="{{ route('user.home') }}" class="text-decoration-none text-secondary link-footer">Beranda</a><br>
+                <a href="{{ route('games.index') }}" class="text-decoration-none text-secondary link-footer">Game</a><br>
+                <a href="{{ route('products.index') }}" class="text-decoration-none text-secondary link-footer">Produk</a><br>
+                <a href="{{ route('auctions.index') }}" class="text-decoration-none text-secondary link-footer">Lelang</a><br>
+                <a href="{{ route('user.orders') }}" class="text-decoration-none text-secondary link-footer">Transaksi</a><br>
+                <a href="{{ route('user.complaints.index') }}" class="text-decoration-none text-secondary link-footer">Komplain</a><br>
             </div>
             <div class="col-md-3 my-3">
-                <h6 class="fw-bold">Pembeli</h6>
-                <a href="#" class="text-decoration-none text-secondary link-footer">Cara Belanja</a><br>
-                <a href="#" class="text-decoration-none text-secondary link-footer">Cara Pembayaran</a><br>
-
-                <h6 class="mt-5 fw-bold">Penjual</h6>
-                <a href="#" class="text-decoration-none text-secondary link-footer">Cara Menjadi Penjual</a><br>
-                <a href="#" class="text-decoration-none text-secondary link-footer">Biaya Berjualan</a><br>
-                <a href="#" class="text-decoration-none text-secondary link-footer">Metode Pencairan Saldo</a><br>
-                <a href="#" class="text-decoration-none text-secondary link-footer">Testimoni Penjual</a><br>
+                <h6 class="fw-bold">Navigasi Penjual</h6>
+                <a href="{{ route('seller.dashboard') }}" class="text-decoration-none text-secondary link-footer">Kelola Toko</a><br>
+                <a href="{{ route('seller.withdraws.index') }}" class="text-decoration-none text-secondary link-footer">Penarikan Saldo</a><br>
+                <a href="{{ route('seller.incoming_orders.index') }}" class="text-decoration-none text-secondary link-footer">Pesanan Pelanggan</a><br>
+                <a href="{{ route('seller.products.index') }}" class="text-decoration-none text-secondary link-footer">Kelola Produk</a><br>
+                <a href="{{ route('seller.auctions.index') }}" class="text-decoration-none text-secondary link-footer">Kelola Lelang Produk</a><br>
+                <a href="{{ route('seller.complaints.index') }}" class="text-decoration-none text-secondary link-footer">Komplain Pelanggan</a><br>
+                <a href="{{ route('seller.reviews.index') }}" class="text-decoration-none text-secondary link-footer">Lihat Ulasan</a><br>
+                <a href="{{ route('seller.transaction-report.index') }}" class="text-decoration-none text-secondary link-footer">Laporan Transaksi</a><br>
             </div>
             <div class="col-md-3 my-3">
                 <h6 class="fw-bold">Metode Pembayaran</h6>
@@ -81,3 +83,22 @@
         </p>
     </div>
 </footer>
+<!-- Terms Conditions Modal -->
+    <div class="modal fade" id="termsConditions" tabindex="-1" aria-labelledby="termsConditionsLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h1 class="modal-title fs-5 fw-bold" id="termsConditionsLabel">Syarat & Ketentuan</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body p-0" style="height: 500px;">
+                <iframe 
+                    src="{{ asset('SyaratDanKetentuanLelangGame.pdf') }}#toolbar=0&navpanes=0&scrollbar=1"
+                    width="100%"
+                    height="100%"
+                    style="border: none;">
+                </iframe>
+        </div>
+        </div>
+    </div>
+    </div>
